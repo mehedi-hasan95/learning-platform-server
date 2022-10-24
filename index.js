de7dbcbc312dfce4389bede1264cf5a1bc6c6a7b
mehedititu <mh.me.mehedi@gmail.com>
2022-10-24 22:31:02 +0600

    add some fake data

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const categories = require('./data/categories.json');
+const news = require('./data/category.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/categories', (req, res) => {
-    res.send(categories);
-  })
+  res.send(categories);
+})
+
+
+app.get('/categories/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(n => n.category_id === id);
+  res.send(selectedNews);
+});
 
 app.listen(port, () => {
   console.log(`The learning platform is runing with: ${port}`)
